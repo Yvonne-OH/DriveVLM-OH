4c438ca19d6ecbc18a9ab4640cdc6c3fd6030797
39829 <398290464@qq.com>
2025-01-10 20:47:23 +0800

    Fix json

--- Util/util.py
+++ Util/util.py
@@ -218,6 +218,7 @@ class DataPreprocessor:
 
 
 
+
         ordered_indices = [1, 0, 2, 4, 3, 5]  # Indices corresponding to the fixed order
         ordered_images = [processed_images[idx] for idx in ordered_indices]
         return ordered_images\
